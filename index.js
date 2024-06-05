8ae756000ae6eda9e25b7f9a8f0641fd990767bd
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-06-05 23:01:03 +0500

    yarmigacha buldim

--- index.js
+++ index.js
@@ -40,7 +40,7 @@
 //   console.log("yolg`on");
 // }
 
-//5-masala
+//6-masala
 // let a = +prompt("a ni qiymatini kiriting");
 // let b = +prompt("b ni qiymatini kiriting");
 // let c = +prompt("c ni qiymatini kiriting");
@@ -50,4 +50,77 @@
 //   console.log("yolg`on");
 // }
 
-//6-masala
+//7-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// let c = +prompt("c ni qiymatini kiriting");
+// if (a < b && b<c) {
+//   console.log("rost");
+// } else {
+//   console.log("yolg`on");
+// }
+
+//8-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// if (a < 0 && b<0) {
+//   console.log(" a va b soni toq son");
+// } else {
+//   console.log("toq son emas");
+// }
+
+//9-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// if (a < 0 || b<0) {
+//   console.log(" a va b soni toq son");
+// } else {
+//   console.log("toq son emas");
+// }
+
+//10-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// if (a < 0 || b<0) {
+//   console.log(" a va b sonlarining faqat bittasi toq son");
+// } else {
+//   console.log("toq son emas");
+// }
+
+//11-masala
+// let a = +prompt("sonni kiriting");
+// if (a > 0) {
+//   a++;
+//   console.log(a);
+//   console.log("birga oshirildi");
+// } else {
+//   console.log("ozgartirilmasin");
+// }
+
+//12-masala
+// let a = +prompt("sonni kiriting");
+// if (a > 0) {
+//   a++;
+//   console.log(a);
+//   console.log("birga oshirildi");
+// } else a < 0;
+// {
+//   console.log(a - 2);
+//   console.log("2 ga kamaytirildi");
+// }
+
+//13-masala
+// let a = +prompt("sonni kiriting");
+// if (a > 0) {
+//   a++;
+//   console.log(a);
+//   console.log("birga oshirildi");
+// } else a < 0;
+// {
+//   a - 2;
+//   console.log(a - 2);
+//   console.log("2 ga kamaytirildi");
+//   if ((a = 0)) {
+//     console.log(a + 10);
+//   }
+// }
