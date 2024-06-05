342ca145217d41d2583042ad42fd483581657da0
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-06-05 22:36:13 +0500

    bosladim

---
+++ index.js
@@ -0,0 +1,53 @@
+//1-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// if (a > 0) {
+//   console.log(" a soni musbat");
+// } else {
+//   console.log("musbat emas");
+// }
+
+//2-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// if (a < 0) {
+//   console.log(" a soni toq son");
+// } else {
+//   console.log("toq son emas");
+// }
+
+//3-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// if (a % 2 == 0) {
+//   console.log(" a soni juft son");
+// } else {
+//   console.log("juft son emas");
+// }
+
+//4-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// if (a >2 &&  b <=3) {
+//   console.log("rost");
+// } else {
+//   console.log("yolg`on");
+// }
+
+//5-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// if (a <= 0 || b < -2) {
+//   console.log("rost");
+// } else {
+//   console.log("yolg`on");
+// }
+
+//5-masala
+// let a = +prompt("a ni qiymatini kiriting");
+// let b = +prompt("b ni qiymatini kiriting");
+// let c = +prompt("c ni qiymatini kiriting");
+// if (a <= b <=c) {
+//   console.log("rost");
+// } else {
+//   console.log("yolg`on");
+// }
+
+//6-masala
